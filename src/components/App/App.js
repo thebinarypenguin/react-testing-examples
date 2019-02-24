2f699506628cbc1bc09d21cdfc0a4766853fed68
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-23 22:22:35 -0500

    Re-org CRA boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <main>
+        <h1>React Testing Examples</h1>
+        <p>IOU one button</p>
+      </main>
+    );
+  }
+}
+
+export default App;
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/*
+ * Smoke Test
+ * When we render the component does it cause any errors?
+ */
 it('renders without crashing', () => {
+
   const div = document.createElement('div');
+
   ReactDOM.render(<App />, div);
+
   ReactDOM.unmountComponentAtNode(div);
 });
